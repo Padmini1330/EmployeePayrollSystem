f2594382bf60378aaa7f0286a923b3156c618903
padmini v <padmini.v@perfios.com>
2021-09-29 01:22:06 +0530

    REFACTORED: employee constructor in Employee class

--- src/main/java/EmployeePayrollSystem/Employee.java
+++ src/main/java/EmployeePayrollSystem/Employee.java
@@ -12,12 +12,37 @@ public class Employee
 	public String address;
 	public String phoneNumber;
 	public String startDate;
-	
-	
-	
+    private Payroll payroll;
+    private Company company;
+
+   
+    public Payroll getPayroll() 
+    {
+        return payroll;
+    }
+
+    public void setPayroll(Payroll payroll) 
+    {
+        this.payroll = payroll;
+    }
+
+    public Employee(int id, int deptID,int companyID,String name, String gender, String address, String phoneNumber, String startDate,Payroll payroll,Company company)
+    {
+        this.employeeID = id;
+        this.departmentID = deptID;
+        this.companyID = companyID;
+        this.employeeName = name;
+        this.gender = gender;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.startDate = startDate;
+        this.payroll=payroll;
+        this.company=company;
+
+    }
+
 	public Employee(int employeeID, int departmentID, int companyID, String employeeName, String gender, String address,
-			String phoneNumber, String startDate) 
-	{
+			String phoneNumber, String startDate) {
 		super();
 		this.employeeID = employeeID;
 		this.departmentID = departmentID;
@@ -28,88 +53,15 @@ public class Employee
 		this.phoneNumber = phoneNumber;
 		this.startDate = startDate;
 	}
-	
 
-
-	public Employee(int id, String name) 
-	{
-		this.employeeID=id;
-		this.employeeName=name;
-	}
-
-
-
-	public int getEmployeeID() 
-	{
-		return employeeID;
-	}
-	public int getDepartmentID() 
-	{
-		return departmentID;
-	}
-	public int getCompanyID() 
-	{
-		return companyID;
-	}
-	public String getEmployeeName() 
-	{
-		return employeeName;
-	}
-	public String getGender()
-	{
-		return gender;
-	}
-	public String getAddress() 
-	{
-		return address;
-	}
-	public String getPhoneNumber() 
-	{
-		return phoneNumber;
-	}
-	public String getStartDate()
-	{
-		return startDate;
-	}
-	public void setEmployeeID(int employeeID)
-	{
-		this.employeeID = employeeID;
-	}
-	public void setDepartmentID(int departmentID) 
-	{
-		this.departmentID = departmentID;
-	}
-	public void setCompanyID(int companyID) 
-	{
-		this.companyID = companyID;
-	}
-	public void setEmployeeName(String employeeName) 
-	{
-		this.employeeName = employeeName;
-	}
-	public void setGender(String gender)
-	{
-		this.gender = gender;
-	}
-	public void setAddress(String address) 
-	{
-		this.address = address;
-	}
-	public void setPhoneNumber(String phoneNumber) 
-	{
-		this.phoneNumber = phoneNumber;
-	}
-	public void setStartDate(String startDate)
-	{
-		this.startDate = startDate;
-	}
 	@Override
-	public String toString() 
-	{
+	public String toString() {
 		return "Employee [employeeID=" + employeeID + ", departmentID=" + departmentID + ", companyID=" + companyID
 				+ ", employeeName=" + employeeName + ", gender=" + gender + ", address=" + address + ", phoneNumber="
-				+ phoneNumber + ", startDate=" + startDate + "]";
+				+ phoneNumber + ", startDate=" + startDate + ", payroll=" + payroll + ", company=" + company + "]";
 	}
-	
-	
+
+    
 }
+	
+
