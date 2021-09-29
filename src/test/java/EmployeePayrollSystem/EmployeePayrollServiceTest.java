1186a3c8cf7ed3d4d509b0bf943aeb72d11c5393
padmini v <padmini.v@perfios.com>
2021-09-29 13:14:41 +0530

    REFACTORED: Added test method to verify the retrieval of employees who have joined in the given range

--- src/test/java/EmployeePayrollSystem/EmployeePayrollServiceTest.java
+++ src/test/java/EmployeePayrollSystem/EmployeePayrollServiceTest.java
@@ -4,7 +4,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -30,7 +30,8 @@ public class EmployeePayrollServiceTest
 	    public void readFromEmployeePayroll_readGivenEmployee() 
 	    {
 	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
-	        long size = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO, "Jim").size();
+	        long size = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO, "Ross").size();
+	        System.out.println(size);
 	        Assert.assertEquals(1, size);
 	    }
 
@@ -38,12 +39,23 @@ public class EmployeePayrollServiceTest
 	    public void whenBasicPay_IsUpdated_ReturnsCorrectValues() 
 	    {
 	        String name = "Jim";
-	        String basicPay = "300";
+	        String basicPay = "3000";
 	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
 	        List<Employee> employeeList = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO, name);
 	        employeePayrollService.updatePayroll(name, basicPay);
-	        boolean result = employeePayrollService.compareUpdate(name);
+	        boolean result = employeePayrollService.compareUpdateSync(name);
 	        Assert.assertTrue(result);
+	    }
+	    
+	    @Test
+	    public void givenDateRange_WhenCorrect_RetrieveEmployeesWhoJoinedInTheGivenRange() 
+	    {
+	        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+	        LocalDate startDate = LocalDate.of(2021, 4, 19);
+	        LocalDate endDate = LocalDate.of(2021, 12, 19);
+	        List<Employee> employeeList = employeePayrollService.readEmployeeJoinedInRange(startDate, endDate);
+	        System.out.println(employeeList.size());
+	        Assert.assertEquals(3, employeeList.size());
 	    }
 
 }
