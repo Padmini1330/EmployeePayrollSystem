51b5731d7644396f5864b1e8bfd642af516a93af
padmini v <padmini.v@perfios.com>
2021-09-29 14:54:48 +0530

    REFACTORED: Changed variable types

--- src/main/java/EmployeePayrollSystem/Employee.java
+++ src/main/java/EmployeePayrollSystem/Employee.java
@@ -15,8 +15,98 @@ public class Employee
     private Payroll payroll;
     private Company company;
 
-   
-    public Payroll getPayroll() 
+    
+    public int getEmployeeID()
+    {
+		return employeeID;
+	}
+
+	public int getDepartmentID()
+	{
+		return departmentID;
+	}
+
+	public int getCompanyID()
+	{
+		return companyID;
+	}
+
+	public String getEmployeeName() 
+	{
+		return employeeName;
+	}
+
+	public String getGender() 
+	{
+		return gender;
+	}
+
+	public String getAddress() 
+	{
+		return address;
+	}
+
+	public String getPhoneNumber() 
+	{
+		return phoneNumber;
+	}
+
+	public LocalDate getStartDate()
+	{
+		return startDate;
+	}
+
+	public Company getCompany()
+	{
+		return company;
+	}
+
+	public void setEmployeeID(int employeeID)
+	{
+		this.employeeID = employeeID;
+	}
+
+	public void setDepartmentID(int departmentID) 
+	{
+		this.departmentID = departmentID;
+	}
+
+	public void setCompanyID(int companyID) 
+	{
+		this.companyID = companyID;
+	}
+
+	public void setEmployeeName(String employeeName)
+	{
+		this.employeeName = employeeName;
+	}
+
+	public void setGender(String gender) 
+	{
+		this.gender = gender;
+	}
+
+	public void setAddress(String address) 
+	{
+		this.address = address;
+	}
+
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+
+	public void setStartDate(LocalDate startDate)
+	{
+		this.startDate = startDate;
+	}
+
+	public void setCompany(Company company) 
+	{
+		this.company = company;
+	}
+
+	public Payroll getPayroll() 
     {
         return payroll;
     }
@@ -42,7 +132,8 @@ public class Employee
     }
 
 	public Employee(int employeeID, int departmentID, int companyID, String employeeName, String gender, String address,
-			String phoneNumber, LocalDate startDate) {
+			String phoneNumber, LocalDate startDate) 
+	{
 		super();
 		this.employeeID = employeeID;
 		this.departmentID = departmentID;
@@ -57,7 +148,8 @@ public class Employee
 	
 
 	@Override
-	public String toString() {
+	public String toString() 
+	{
 		return "Employee [employeeID=" + employeeID + ", departmentID=" + departmentID + ", companyID=" + companyID
 				+ ", employeeName=" + employeeName + ", gender=" + gender + ", address=" + address + ", phoneNumber="
 				+ phoneNumber + ", startDate=" + startDate + ", payroll=" + payroll + ", company=" + company + "]";
